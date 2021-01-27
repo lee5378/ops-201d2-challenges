966112e3211e4d2a23c6ba261e937a5db418adfc
lee5378 <lee5378@alumni.uidaho.edu>
2021-01-27 17:47:48 +0000

    script updated

--- mygreatscript.sh
+++ mygreatscript.sh
@@ -1,1 +1,16 @@
-echo "Hello world"
+#!/bin/bash
+
+# Name: David Lee
+# Script: Ops-201d2 Class 2
+# Date: 1/27/2021
+# Purpose: Network info
+
+# Variables
+
+network="ip a"
+
+# Main
+
+$network
+
+$network>outfile.txt
